034fd635dedac2ad63afa588a65e4b92b9eee6b0
ladynobeard <syh031@ucsd.edu>
2019-01-11 11:40:03 -0800

    initial push

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
 
+// Global Variabls
+const countriesList = document.getElementById("countries");
+let countries; // will contain "fetched" data
+
 window.addEventListener('load', init);
+
+function init() {
+	fetch("https://restcountries.eu/rest/v2/all")
+	.then(res => res.json())
+	.then(data => initialize(data))
+	.catch(err => console.log("Error:", err));
+
+}
 
 /*
  * init()
  * Called by window.addEventListener('load', init);
  */
-function init() {
+function initialize(countriesData) {
+	
+	countries = countriesData;
+	console.log("First country is: " + countries[0].name);
+	
+	
 	document.body = document.createElement("body");
 	document.body.innerHTML = "<p>Hello World!</p>";
 }
